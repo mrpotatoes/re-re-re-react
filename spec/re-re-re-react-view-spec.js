013d3b8ee2ea50c304e7fee705bd4bc8e793558f
Andric Villanueva <contact@andricvillanueva.com>
2017-04-16 11:21:50 -0400

    Adding in default settings; gotta apply

--- spec/re-re-re-react-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ReReReReactView from '../lib/re-re-re-react-view';
-
-describe('ReReReReactView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
